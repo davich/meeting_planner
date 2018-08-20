462b3d02a9feead39a1810038bd59c0d20e546d5
NLT <NamibiaLT@users.noreply.github.com>
2018-08-19 20:54:34 -0700

    add class to tests

--- planner_spec.rb
+++ planner_spec.rb
@@ -1,27 +1,29 @@
 require 'rspec'
 require_relative 'planner'
 
-RSpec.describe 'meeting_planner' do
-  let(:overlapping_timeslots) {
-    slotsA = [[10, 50], [60, 120], [140, 210]],
-    slotsB = [[0, 15], [60, 70]],
-    dur = 8
-  }
+RSpec.describe 'Planner' do
+  describe 'meeting_planner' do
+    let(:overlapping_timeslots) {
+      slotsA = [[10, 50], [60, 120], [140, 210]],
+      slotsB = [[0, 15], [60, 70]],
+      dur = 8
+    }
 
-  let(:no_overlapping_timeslots) {
-    slotsA = [[10, 50], [60, 120], [140, 210]],
-    slotsB = [[0, 15], [60, 70]],
-    dur = 12
-  }
-  context 'overlap' do
-    it 'returns a timeslot start and end time' do
-      expect(overlapping_timeslots.meeting_planner).to eq([60, 68])
+    let(:no_overlapping_timeslots) {
+      slotsA = [[10, 50], [60, 120], [140, 210]],
+      slotsB = [[0, 15], [60, 70]],
+      dur = 12
+    }
+    context 'overlap' do
+      it 'returns a timeslot start and end time' do
+        expect(overlapping_timeslots.meeting_planner).to eq([60, 68])
+      end
     end
-  end
 
-  context 'no overlap' do
-    it 'returns null if there no overlap between time slots' do
-      expect(no_overlapping_timeslots.meeting_planner).to eq(null)
+    context 'no overlap' do
+      it 'returns null if there no overlap between time slots' do
+        expect(no_overlapping_timeslots.meeting_planner).to eq(null)
+      end
     end
   end
 end
