9b475201d8c7c9b6bff8b35aff456b60c3fc7e61
David Carlin <david.carlin@cultureamp.com>
2018-08-21 10:09:44 +1000

    Remove redundant comments

--- lib/planner.rb
+++ lib/planner.rb
@@ -9,19 +9,15 @@ class Planner
     index_a = 0
     index_b = 0
     while index_a < slots_a.size && index_b < slots_b.size
-      #if the start time of slot A is greater than the end time of slot B, then increment the index of slot B
       if slot_a_start(index_a) > slot_b_end(index_b)
         index_b += 1
-      # elsif the start time of slot B is greater the end time of slot A, increment the index of A
       elsif slot_b_start(index_b) > slot_a_end(index_a)
         index_a += 1
       else
-        # overlap = the smallest end time - the largest start time
         if overlap_size(index_a, index_b) >= dur
           largest_start = largest_start_time(index_a, index_b)
           return [largest_start, largest_start + dur]
         else
-          # move on the the next index in the slot with the smallest end time
           if slot_a_end(index_a) < slot_b_end(index_b)
             index_a += 1
           else
