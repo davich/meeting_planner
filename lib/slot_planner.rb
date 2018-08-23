9c1c7e5205e8926ec159f910d655b2334930ce35
David Carlin <david.carlin@cultureamp.com>
2018-08-23 13:13:50 +1000

    Added slot class to remove some methods from slot_planner

--- lib/slot_planner.rb
+++ lib/slot_planner.rb
@@ -1,14 +1,16 @@
+require 'slot'
+
 class SlotPlanner
-  def initialize(slot_a, slot_b, dur)
-    @slot_a = slot_a
-    @slot_b = slot_b
-    @dur = dur
+  def initialize(slot_a, slot_b, duration)
+    @slot_a = Slot.new(slot_a)
+    @slot_b = Slot.new(slot_b)
+    @duration = duration
   end
 
   def plan
     return { increment_index_a: true } if slot_b_starts_after_slot_a_ends?
     return { increment_index_b: true } if slot_a_starts_after_slot_b_ends?
-    return { slot_found: first_overlap_of_dur } if large_enough_overlap?
+    return { slot_found: first_overlap_of_duration } if large_enough_overlap?
 
     if slot_a_ends_first?
       { increment_index_a: true }
@@ -19,26 +21,26 @@ class SlotPlanner
 
   private
 
-  attr_reader :dur, :slot_a, :slot_b
+  attr_reader :duration, :slot_a, :slot_b
 
   def slot_a_starts_after_slot_b_ends?
-    slot_a_start > slot_b_end
+    slot_a.start > slot_b.end
   end
 
   def slot_b_starts_after_slot_a_ends?
-    slot_b_start > slot_a_end
+    slot_b.start > slot_a.end
   end
 
   def large_enough_overlap?
-    overlap_size >= dur
+    overlap_size >= duration
   end
 
-  def first_overlap_of_dur
-    [largest_start_time, largest_start_time + dur]
+  def first_overlap_of_duration
+    [largest_start_time, largest_start_time + duration]
   end
 
   def slot_a_ends_first?
-    slot_a_end < slot_b_end
+    slot_a.end < slot_b.end
   end
 
   def overlap_size
@@ -46,26 +48,10 @@ class SlotPlanner
   end
 
   def largest_start_time
-    [slot_a_start, slot_b_start].max
+    [slot_a.start, slot_b.start].max
   end
 
   def smallest_end_time
-    [slot_a_end, slot_b_end].min
-  end
-
-  def slot_a_start
-    slot_a[0]
-  end
-
-  def slot_a_end
-    slot_a[1]
-  end
-
-  def slot_b_start
-    slot_b[0]
-  end
-
-  def slot_b_end
-    slot_b[1]
+    [slot_a.end, slot_b.end].min
   end
 end
